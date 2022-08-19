d49bf97e7f04bb1936861d499fe887f54678410b
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-08-18 22:54:51 -0500

    PWA Service Worker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './styles.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { CalendarApp } from './CalendarApp';
 
+import './styles.css';
+
 const root = document.getElementById('root');
-ReactDOM.render(
-    <CalendarApp />,
-    root
-);
+ReactDOM.render(<CalendarApp />, root);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
