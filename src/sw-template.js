4a0c6a1f99f37ed07066d41c2d0c83b8056fe50b
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-08-28 23:35:43 -0500

    put, delete, post

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,9 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
+workbox.loadModule('workbox-background-sync');
+
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
+
+registerRoute(new RegExp('http://localhost:4000/api/auth/renew'), new CacheFirst());
+
+registerRoute(new RegExp('http://localhost:4000/api/events'), new CacheFirst());
 
 registerRoute(
   new RegExp('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css'),
@@ -13,4 +20,33 @@ registerRoute(
 registerRoute(
   new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
   new CacheFirst()
+);
+
+// Offline Posts
+const bgSyncPlugin = new BackgroundSyncPlugin('offline-post', {
+  maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
+});
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'POST'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'PUT'
+);
+
+registerRoute(
+  new RegExp('http://localhost:4000/api/events/'),
+  new NetworkOnly({
+    plugins: [bgSyncPlugin],
+  }),
+  'DELETE'
 );
